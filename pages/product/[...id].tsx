d677c4f8ac476947fb82416d89604c75743f5293
christocarr <christocarr@outlook.com>
2023-01-16 17:37:19 +0000

    wip: add dynamic product page

--- pages/product/[...id].tsx
+++ pages/product/[...id].tsx
@@ -1,15 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/router';
 import { ParsedUrlQuery } from 'querystring';
-import { ProductCard, ProductImage } from '../../components';
-
+import { ProductCard, ProductImage, Button } from '../../components';
 import { prisma } from '../../lib/prisma';
+import { IProduct } from '../../types/product';
+import { useAppContext } from '../../context/state';
 
 export default function Page({ product }: InferGetServerSidePropsType<GetServerSideProps>) {
-	console.log(product);
+	const { addToCart } = useAppContext();
+
+	const router = useRouter();
+
+	const handleAddToCart = (product: IProduct) => {
+		addToCart(product);
+	};
+
 	return (
-		<ProductCard>
-			<ProductImage image={product.image} name={product.name} />
-		</ProductCard>
+		<>
+			<ProductCard>
+				<ProductImage image={product.image} name={product.name} />
+			</ProductCard>
+			<Button
+				onClick={() => handleAddToCart(product)}
+				className='py-2 px-4  w-full font-bold text-white bg-black rounded hover:bg-gray-700'
+			>
+				Buy
+			</Button>
+			<Link href='/checkout'>Go to checkout</Link>
+		</>
 	);
 }
 
