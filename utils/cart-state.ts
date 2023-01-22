ae0e1bef60240b5bb434eb8138929602f804f989
christocarr <christocarr@outlook.com>
2023-01-22 12:52:40 +0000

    fix: add return types to functions

--- utils/cart-state.ts
+++ utils/cart-state.ts
@@ -1,13 +1,13 @@
 import { Cart, Item } from '../types/cart';
 
-export const filteredItems = (cart: Cart, productId: string) => {
+export const filteredItems = (cart: Cart, productId: string): Item[] => {
 	return cart.items.filter(({ item }) => item.id !== productId);
 };
 
-export const slicedCartItems = (cart: Cart) => {
+export const slicedCartItems = (cart: Cart): Item[] => {
 	return cart.items.slice();
 };
 
-export const itemIndex = (items: Item[], id: string) => {
+export const itemIndex = (items: Item[], id: string): number => {
 	return items.findIndex(({ item }) => item.id === id);
 };
