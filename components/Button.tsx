c5ad57a10e857f9aa1f98c2ea74bf6b67ad65afd
christocarr <christocarr@outlook.com>
2023-01-23 18:00:09 +0000

    wip: add html intput type attribute for type props and disable type

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,17 @@
 type ButtonProps = {
 	text: React.ReactNode;
+	type?: React.HTMLInputTypeAttribute;
+	disabled?: boolean;
 	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+	className: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
-	return <button onClick={onClick}>{text}</button>;
+const Button: React.FC<ButtonProps> = ({ text, type, disabled, onClick, className }) => {
+	return (
+		<button onClick={onClick} className={className}>
+			{text}
+		</button>
+	);
 };
 
 export default Button;
