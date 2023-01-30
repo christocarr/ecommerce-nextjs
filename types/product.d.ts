246b394dcc9b9088a02828d0da6c0579370095ae
christocarr <christocarr@outlook.com>
2023-01-30 16:44:19 +0000

    chore: add deal field to type and update prisma schema

--- types/product.d.ts
+++ types/product.d.ts
@@ -3,6 +3,7 @@ import { ProductsSpecification } from './specification';
 export interface IProduct {
 	id: string;
 	category: string;
+	deal: boolean;
 	description: string;
 	image: string;
 	name: string;
